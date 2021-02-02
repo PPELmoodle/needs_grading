1f734c1fc05be0f1c4259db11e1617173cae0783
yF-B <64614004+yF-B@users.noreply.github.com>
2021-02-02 14:42:00 +0100

    add  string for 'needs_grading:addinstance'

--- lang/en/block_needs_grading.php
+++ lang/en/block_needs_grading.php
@@ -4,4 +4,5 @@ $string['needs_grading'] = 'Needs Grading';
 $string['activity_modules:addinstance'] = 'Add a new not graded assignments list block';
 $string['activity_modules:myaddinstance'] = 'Add a new not graded assignments list block to the My Moodle page';
 $string['noneedsgrading'] = 'All assignments have been graded.';
+$string['needs_grading:addinstance'] = 'Add a new Needs Grading block';
  ?>
