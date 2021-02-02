e59485b6fc7a580572774a6e46ec7dbfc340084b
yF-B <64614004+yF-B@users.noreply.github.com>
2021-02-02 14:44:27 +0100

    update version from '2020110900' to '2020110901'

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_needs_grading';
-$plugin->version = 2020110900;
+$plugin->version = 2020110901; 
 $plugin->requires = 2020110300;
  ?>
