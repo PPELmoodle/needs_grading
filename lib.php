8437f28a12e334c872b2c7b9539267be3c21b4af
g-jih <rhakdnj47@gmail.com>
2021-02-17 22:35:33 +0100

    modify for the case teamsubmission

--- lib.php
+++ lib.php
@@ -5,42 +5,38 @@ function get_submissions_need_grading($courseid) {
   $sql = "SELECT name, cmid, COUNT(*) as count
             FROM (SELECT DISTINCT a.name AS name, cm.id AS cmid, grm.groupid
                     FROM {assign_submission} asb
-                    JOIN {assign} a      ON a.id = asb.assignment
+                    JOIN {assign} a ON a.id = asb.assignment
                     JOIN {course_modules} cm ON cm.instance = a.id
-                    JOIN {modules} md        ON md.id = cm.module
-                    JOIN {user} u            ON u.id = asb.userid
-                    JOIN {groups_members} grm                        ON grm.userid = u.id
-                    JOIN {groups} gr                                        ON gr.id = grm.groupid
-                    LEFT JOIN {assign_user_mapping} um ON um.userid = u.id AND um.assignment = a.id
+                    JOIN {modules} md ON md.id = cm.module
+                    JOIN {user} u ON asb.userid = asb.userid
+                    JOIN {groups_members} grm ON grm.userid = asb.userid
+                    JOIN {groups} gr  ON gr.id = grm.groupid
+                    LEFT JOIN {assign_user_mapping} um ON um.userid = asb.userid AND um.assignment = a.id
                     LEFT JOIN {assign_grades} asg ON asg.userid = asb.userid AND asg.assignment = asb.assignment
                     WHERE
-                    asb.latest = 1 AND
                     a.course = :courseid1 AND
+                    gr.courseid = a.course AND
                     md.name = 'assign' AND
                     asb.status = 'submitted' AND
                     cm.deletioninprogress = 0 AND
                     a.teamsubmission = 1 AND
-                    (asg.userid is NULL OR asg.assignment is NULL OR asg.grade is NULL)
-                    UNION
-                    SELECT a.name AS name, cm.id AS cmid, grm.groupid
+                    (asg.grade is NULL OR asg.grade < 0)) AS teamsub
+                    group by name, cmid
+              UNION
+              SELECT a.name AS name, cm.id AS cmid, COUNT(*) as count
                     FROM {assign_submission} asb
-                    JOIN {assign} a      ON a.id = asb.assignment
+                    JOIN {assign} a ON a.id = asb.assignment
                     JOIN {course_modules} cm ON cm.instance = a.id
-                    JOIN {modules} md        ON md.id = cm.module
-                    JOIN {user} u            ON u.id = asb.userid
-                    JOIN {groups_members} grm  ON grm.userid = u.id
-                    JOIN {groups} gr        ON gr.id = grm.groupid
-                    LEFT JOIN {assign_user_mapping} um ON um.userid = u.id AND um.assignment = a.id
+                    JOIN {modules} md ON md.id = cm.module
                     LEFT JOIN {assign_grades} asg ON asg.userid = asb.userid AND asg.assignment = asb.assignment
                     WHERE
-                    asb.latest = 1 AND
                     a.course = :courseid2 AND
                     md.name = 'assign' AND
                     asb.status = 'submitted' AND
                     cm.deletioninprogress = 0 AND
                     a.teamsubmission = 0 AND
-                    (asg.userid is NULL OR asg.assignment is NULL OR asg.grade is NULL)) AS dist
-                    GROUP BY name, cmid";
+                    (asg.grade is NULL OR asg.grade < 0)
+                    GROUP BY a.name, cm.id";
   $params = [
       'courseid1' => $courseid,
       'courseid2' => $courseid
