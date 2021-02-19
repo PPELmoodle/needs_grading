42a41bbb6e0ecc2f9b75ea2609f400c1319e1e75
yF-B <64614004+yF-B@users.noreply.github.com>
2021-02-19 14:36:46 +0100

    add function to count sunbmission for a group

--- lib.php
+++ lib.php
@@ -42,4 +42,78 @@ function get_submissions_need_grading($courseid) {
 
   return $DB->get_recordset_sql($sql, $params);
 }
+function get_submissions_need_grading_for_my_group($courseid,$groupid){
+  global $DB;
+  
+  $sql = "SELECT assignmentname, cmid, count
+           FROM (SELECT  assignmentname, cmid, COUNT(*) as count, assignid 
+                  FROM (SELECT cm.id AS cmid, a.name AS assignmentname, a.id AS assignid
+                   FROM {assign_submission} asb
+                   JOIN {assign} a ON a.id = asb.assignment
+                   LEFT JOIN {assign_grades} ag ON ag.assignment = a.id
+                                                   AND asb.assignment = ag.assignment
+                                                   AND asb.userid = ag.userid
+                                                   AND asb.attemptnumber = ag.attemptnumber
+                   JOIN {user} u ON u.id = asb.userid AND u.deleted = 0
+                   JOIN {course} c ON c.id = :courseid1 AND c.id = a.course
+                   JOIN {course_modules} cm ON a.course = cm.course AND cm.instance = a.id 
+                   JOIN {modules} md ON md.id = cm.module
+                   JOIN {grade_items} gi ON a.course = gi.courseid AND gi.itemmodule = 'assign' AND a.id = gi.iteminstance
+                   LEFT JOIN {groups_members} grm  ON grm.userid = asb.userid
+                   LEFT JOIN {groups} gr  ON gr.id = grm.groupid
+                   WHERE
+                   asb.latest = 1
+                   AND asb.timemodified IS NOT NULL
+                   AND a.teamsubmission = 0
+                   AND asb. STATUS = 'submitted'
+                   AND md.name = 'assign' 
+                   AND grm.groupid = :groupid1
+                   AND (asb.timemodified >= ag.timemodified
+                        OR ag.timemodified IS NULL
+                        OR ag.grade IS NULL)
+                   Group By u.id, a.id) AS team 
+                   Group By team.assignid
+             UNION
+             SELECT assignmentname, cmid, COUNT(*) AS count, assignid
+               FROM (SELECT assignmentname, assignid, groupid,cmid
+                      FROM (SELECT cm.id AS cmid, a.name AS assignmentname, a.id AS assignid, u.id AS userid, grm.groupid AS groupid
+                             FROM {assign_submission} asb
+                             JOIN {assign} a ON a.id = asb.assignment
+                             LEFT JOIN {assign_grades} ag ON ag.assignment = a.id
+                                                          AND asb.assignment = ag.assignment
+                                                          AND asb.userid = ag.userid
+                                                          AND asb.attemptnumber = ag.attemptnumber
+                             JOIN {user} u ON u.id = asb.userid AND u.deleted = 0
+                             JOIN {course} c ON c.id = :courseid2 AND c.id = a.course
+                             JOIN {course_modules} cm ON a.course = cm.course AND cm.instance = a.id 
+                             JOIN {modules} md ON md.id = cm.module
+                             JOIN {grade_items} gi ON a.course = gi.courseid AND gi.itemmodule = 'assign' AND a.id = gi.iteminstance
+                             LEFT JOIN {groups_members} grm  ON grm.userid = asb.userid
+                             LEFT JOIN {groups} gr  ON gr.id = grm.groupid
+                             WHERE
+                             asb.latest = 1
+                             AND asb.timemodified IS NOT NULL
+                             AND a.teamsubmission = 1
+                             AND asb. STATUS = 'submitted'
+                             AND md.name = 'assign' 
+                             AND grm.groupid = :groupid2
+                             AND (asb.timemodified >= ag.timemodified
+                                  OR ag.timemodified IS NULL 
+                                  OR ag.grade IS NULL)
+                             AND grm.groupid IS NOT NULL 
+                             Group By grm.groupid,a.id) AS team 
+                             Group BY team.assignid, team.userid) AS sum 
+                             Group BY sum.assignid) AS assignmenglist
+                             ORDER BY assignmenglist.assignid";
+    
+    $params = [
+        'courseid1' => $courseid,
+        'groupid1' => $groupid,
+        'courseid2' => $courseid,
+        'groupid2' => $groupid
+    ];
+    
+     return $DB->get_recordset_sql($sql, $params);
+}
+
  ?>
