11e46ea878d868fefd9391087542e5f201760d6a
yF-B <64614004+yF-B@users.noreply.github.com>
2021-02-19 14:07:46 +0100

    add string to "my group"

--- lang/en/block_needs_grading.php
+++ lang/en/block_needs_grading.php
@@ -4,4 +4,5 @@ $string['needs_grading'] = 'Needs Grading';
 $string['activity_modules:addinstance'] = 'Add a new not graded assignments list block';
 $string['noneedsgrading'] = 'All assignments have been graded.';
 $string['needs_grading:addinstance'] = 'Add a new Needs Grading block';
+$string['my_group'] = 'My group';
  ?>
